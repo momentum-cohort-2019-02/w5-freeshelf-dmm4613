cb0d6a2908344390801c28fbd2b52d54c6f34423
Dan Miller <dmm4613@gmail.com>
2019-03-13 15:17:07 -0400

    base website is working. can go to specific categories.

--- core/views.py
+++ core/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from core.models import Book, Author, Category
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 class CategoriesListView(generic.ListView):
     model = Category
 
 class CategoriesDetailView(generic.DetailView):
     model = Category
-    
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(CategoriesDetailView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['category_list'] = Category.objects.all()
+        return context
+
+
 def index(request):
     """View function for home page of site."""
 
