ba4ad9f70082ccd5f01289309a63e5043f0cb921
Dan Miller <dmm4613@gmail.com>
2019-03-14 10:45:37 -0400

    moved a few things around

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 
 ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_REDIRECT_URL = '/'
+
+
+django_heroku.settings(locals())
+#nothing below this line
